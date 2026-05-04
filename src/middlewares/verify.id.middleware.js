935185bc21da35f47230623b3e9a004c75718343
PedrocaSolador <pedroclashpx@gmail.com>
2026-05-04 17:43:54 -0300

    codigos

---
+++ src/middlewares/verify.id.middleware.js
@@ -0,0 +1,12 @@
+// Middleware para Validação de um id
+const verifyParamId = (request, response, next) => {
+  const id = request.params.id;
+  if (isNaN(id)) {
+    return response.status(400).json({
+      message: 'Erro: Id deve ser um número!',
+    });
+  }
+  next();
+};
+
+export default verifyParamId;
